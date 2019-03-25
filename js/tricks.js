31e3bb3e7df4f6f6abae67bcdf27d80127509174
gleyconxavier <gleyconxcarlos@gmail.com>
2019-03-25 00:56:02 -0400

    Big Update

---
+++ js/tricks.js
@@ -0,0 +1,18 @@
+// Animation Reset
+
+alert("Site em construção.")
+
+function aniTitle(who) {
+    const status = document.getElementsByClassName('animated')
+
+    let ele = status.item(who)
+
+    let newone = ele.cloneNode(true);
+    ele.parentNode.replaceChild(newone, ele);
+    
+};
+
+function showNav() {
+    document.getElementById("navbar").classList.toggle("show");
+    document.getElementById("changer").classList.toggle("fa-times");
+  }
