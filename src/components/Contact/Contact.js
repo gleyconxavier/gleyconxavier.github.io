479fa7b40f09316ca336ad3f71f524005519df10
gleyconxavier <gleyconxcarlos@gmail.com>
2019-11-23 14:38:30 -0400

    Upload

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -31,15 +31,18 @@ const About = () => {
                                 Contatos:
                             </h4>
                             <a href="https://api.whatsapp.com/send?phone=069984068936&text=Olá%20Gleycon%20vi%20seu%20portfólio%20vamos%20conversar?"
-                                 className="no-underline near-white bg-animate bg-green hover-bg-gray inline-flex pointer items-center ma2 tc br2 pa2">
+                                 className="no-underline near-white bg-animate bg-green hover-bg-gray inline-flex pointer items-center ma2 tc br2 pa2"
+                                 target="_blank" rel="noopener noreferrer">
                                 <span className="f6 ml3 pr2">Whatsapp</span>
                             </a>
                             <a href="https://api.whatsapp.com/send?phone=069984068936&text=Olá%20Gleycon%20vi%20seu%20portfólio%20vamos%20conversar?"
-                                 className="no-underline near-white bg-animate bg-black hover-bg-gray inline-flex pointer items-center ma2 tc br2 pa2">
+                                 className="no-underline near-white bg-animate bg-black hover-bg-gray inline-flex pointer items-center ma2 tc br2 pa2"
+                                 target="_blank" rel="noopener noreferrer" >
                                 <span className="f6 ml3 pr2">Email</span>
                             </a>
                             <a href="https://www.linkedin.com/in/gleyconxavier/"
-                                 className="no-underline near-white bg-animate bg-blue hover-bg-gray inline-flex pointer items-center ma2 tc br2 pa2">
+                                 className="no-underline near-white bg-animate bg-blue hover-bg-gray inline-flex pointer items-center ma2 tc br2 pa2"
+                                 target="_blank" rel="noopener noreferrer" >
                                 <span className="f6 ml3 pr2">LinkedIn</span>
                             </a>
                         </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Navigation.css';
+import {Link} from 'react-router-dom';
+
 import logo from '../../logo.svg';
 import {FaGithubAlt} from 'react-icons/fa';
 import {FaLinkedin} from 'react-icons/fa';
@@ -18,16 +19,16 @@ const Navigation = () => {
         <nav>
 
             <RotInUpRight>
-                <header class="sans-serif">
-                    <div class="bg-black-30 dt w-100 center"> 
-                        <div class="dtc w2 v-mid pa3">
-                            <a href="/" class="dib w2 h2 ba b--white-90 grow-large border-box">
+                <header className="sans-serif">
+                    <div className="bg-black-30 dt w-100 center"> 
+                        <div className="dtc w2 v-mid pa3">
+                            <Link to="/" className="dib w2 h2 ba b--white-90 grow-large border-box">
                             <img src={logo} className="App-logo" alt="logo" />
-                            </a>
+                            </Link>
                         </div>
-                        <div class="dtc v-mid b3-white tr pa2">
-                            <a class="f3 f1-ns fw4 hover-white no-underline white-70 dn dib pv2 ph3" href="/" ><FaGithubAlt /></a> 
-                            <a class="f3 f1-ns fw4 hover-white no-underline white-70 dn dib pv2 ph3" href="/" ><FaLinkedin /></a> 
+                        <div className="dtc v-mid b3-white tr pa2">
+                            <a className="f3 f1-ns fw4 hover-white no-underline white-70 dn dib pv2 ph3" target="_blank" rel="noopener noreferrer" href="https://github.com/gleyconxavier" ><FaGithubAlt /></a> 
+                            <a className="f3 f1-ns fw4 hover-white no-underline white-70 dn dib pv2 ph3" target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/gleyconxavier" ><FaLinkedin /></a> 
                         </div>
                     </div>
                 </header>
