f9b0ef5ae642a7a4a5d7e47117e0e0667206cc84
gleyconxavier <gleyconxcarlos@gmail.com>
2019-11-25 18:03:01 -0400

    Test | Animations performance

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-import {flipInY} from 'react-animations';
-
-const flipinAnimation = keyframes`${flipInY}`;
-
-const FlipinDiv = styled.div`
-  animation: 1s ${flipinAnimation};
-`;
 
 const Home = () => {
     return (
 
         <div className="tc mt4 mt5-m mt6-l ph4 mw9-ns center">
-            <FlipinDiv><h1 className="f1 fw2 white-90 pv1 bg-black-80 mb0 lh-title">Gleycon <span className="white-50">Xavier</span></h1></FlipinDiv>
-            <FlipinDiv><h2 className="fw1 f3 white-70 mt3 pv1 bg-black-80 mb4">Web Developer</h2></FlipinDiv>
+            <h1 className="f1 animated flipInY fw2 white-90 pv1 bg-black-80 mb0 lh-title">Gleycon <span className="white-50">Xavier</span></h1>
+            <h2 className="fw1 animated flipInY f3 white-70 mt3 pv1 bg-black-80 mb4">Web Developer</h2>
         </div>
     )
 }
